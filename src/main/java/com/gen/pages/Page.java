d29ebcbd3e5927d445f8cf920bbe6da7fb2d5e4a
yogisgit <yogeshravi2704@gmail.com>
2022-06-05 22:19:19 +0530

    added driver and wait in page class

--- src/main/java/com/gen/pages/Page.java
+++ src/main/java/com/gen/pages/Page.java
@@ -1,6 +1,9 @@
 package com.gen.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class Page {
 	WebDriver driver;
-	webDriverWait wait;
+	WebDriverWait wait;
 }
